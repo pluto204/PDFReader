1831e6d5dac3db59e7c463bc9dbdfd729be8a33f
AtheS21 <hadanglinh21@gmail.com>
2016-12-04 22:34:32 +0700

    main test

--- src/pdfreader/Main.java
+++ src/pdfreader/Main.java
@@ -1,23 +1,27 @@
 package pdfreader;
 
+import java.awt.print.PrinterJob;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-import org.apache.pdfbox.pdmodel.PDDocument; 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
 import org.apache.pdfbox.text.PDFTextStripper; 
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//Test automata
+		
 		Automata au = new Automata();
 		Encoder ec = new Encoder();
-		String test = new String("abc.");
-		test = test.replaceAll("[().,!?]", "");
 		
-		System.out.println(test);
-		/*
+		
 		au.loadAutomata();
+		
 		try{
 			PDDocument document = null;
 			document = PDDocument.load(new File("Lão Hạc.pdf"));
@@ -26,22 +30,29 @@ public class Main {
 		        PDFTextStripperByArea stripper = new PDFTextStripperByArea();
 		        stripper.setSortByPosition(true);
 		        PDFTextStripper Tstripper = new PDFTextStripper();
-		        String st = Tstripper.getText(document);
-		        //System.out.println(st);
-		        Scanner sc = new Scanner(st);
-		        //*********Test chinh ta***********************
+		        String s = Tstripper.getText(document);
+		        //s = s.replaceAll("\n[^\t]", " ");
+		        NGramModel model = new NGramModel("resources/VNsyl.txt");
+		        //StringTokenizer st = new StringTokenizer(, ",.\n ");
+		        ArrayList<String> wrong = model.checkSentence("Nguyễn hà Đăng Linh đang đi chơi clgt".toLowerCase(), au);
+		        for(int i = 0; i < wrong.size(); i++) {
+		        	System.out.println(wrong.get(i));
+		        }
+		        
+		        //System.out.println(s);
+		        //Scanner sc = new Scanner(st);
+		        /**********Test chinh ta***********************
 		        String[] test = {"a dua", "lão", "hạc", "cló", "sầt"};
 		        for(int i = 0; i<5; i++){
 		        	String s = test[i];
 		        	s = s.toLowerCase();
 		        	System.out.println(au.recognizeWord(ec.encode(s)));
-		        	
-		        }
+		        8	
+		        }*/
 		    }
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		*/
 		//***********************Test xay dung automata************************
 		/*
 		BufferedReader br = null;
@@ -50,13 +61,15 @@ public class Main {
 			String sCurrentLine;
 			Encoder ec = new Encoder();
 			Automata au = new Automata();
-			br = new BufferedReader(new FileReader("resources/Viet11K.txt"));
+			br = new BufferedReader(new FileReader("resources/VNsyl.txt"));
+			br.readLine();
 			//building the automata
 			while ((sCurrentLine = br.readLine()) != null) {
-				int[] code = ec.encode(sCurrentLine);
+				int[] code = ec.encode(Utilities.getVowelComb(sCurrentLine.toLowerCase()));
 				//sCurrentLine.replaceFirst(".", "");
 				au.addWord(code);
 			}
+			System.out.println(au.size);
 			au.saveAutomata();
 
 		} catch (IOException e) {
@@ -67,8 +80,8 @@ public class Main {
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
-		}*/
-
+		}
+	*/
 	}
 
 }
